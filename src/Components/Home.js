baf2b6ede8a9ac826eb0b3610733f90ced8100c1
Akiko Green <agreen@alphaworks.tech>
2022-04-26 10:52:29 -0700

    feat(setup HomePage): Sets up the Text for the home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+//stlesw
+import { WelcomeBannerContainer, Container, NameHeader, QuoteContainer } from '../Styles-Components/HomeStyles'
 const Home = () => {
   return (
-    <div>Home</div>
+    <Container>
+    <NameHeader>Akiko Green</NameHeader>
+    <WelcomeBannerContainer>
+    /*have this banner moving somehow*/
+    <h2>Welcome To My Website</h2>
+    </WelcomeBannerContainer>
+    <QuoteContainer>
+    <p>A Soft Woman Is A Wolf Caught In Meditation</p>
+    </QuoteContainer>
+    </Container>
   )
 }
 
---
+++ src/Styles-Components/HomeStyles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+
+height: auto;
+width: auto;
+border:2px solid black ;
+  /* background-image:${props => props.backgroundImage || "https://www.outfrontmedia.com/-/media/images/ofm/markets/san-francisco/san-francisco-hero.jpg"}; */
+`
+
+export const NameHeader = styled.h1`
+/* border: 3px solid #CA8F36; */
+-webkit-text-stroke: 2px #CA8F36;
+font-family:'Leckerli One' cursive;
+font-size:400 ;
+`
+
+export const WelcomeBannerContainer = styled.div`
+  font-family:'Leckerli One' cursive;
+font-size:100 ;
+`
+
+export const QuoteContainer = styled.div`
+
+`
